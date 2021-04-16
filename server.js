c486c9b9850411319cfdef2a7efc74a7cc5bb30e
Cal Withington <scwithington@gmail.com>
2021-04-16 17:52:46 -0400

    struggling to get notes.html to page

--- server.js
+++ server.js
@@ -1,23 +1,42 @@
 const path = require('path');
 const express = require('express');
-var db = require('./db/db.json')
-const uuid = require('uuid')
+var db = require('./db/db.json');
+const uuid = require('uuid');
+const fs = require('fs');
 
 const app = express();
 
-app.use(express.json);
-app.use(express.static(path.join(__dirname, 'public')));
+var root = {root: '/Develop/public' + __dirname }
 
-app.get('./public/notes.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Develop'))
+app.use(bodyParser.urlencoded({ extend: false }));
+app.use(express.json);
+app.use(express.static(path.join(__dirname, '/Develop/public')));
+app.get('/', (req, res) => res.sendFile('index.html'));
+app.get('/notes', (req, res) => res.sendFile('notes.html'));
+
+// app.get('./public/notes.html', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'Develop'))
+// })
+
+// get notes
+app.get('/api/notes', (req, res) => {
+    const getNote = db.some((note) => {
+        note.id == parseInt(req.params.id)
+    });
+    let json = getJSON();
+    console.log(json);
+    res.parse(json);
 })
 
-// app.get('./api/notes/:id', (req, res) => {
-    
-// })
+function getJSON() {
+    var data = fs.readFileSync(__dirname + './db/db.json')
+    var dataParse = JSON.parse(data);
+    return dataParse;
+}
 
 //Post a new note
 app.post('./api/notes', (req, res) => {
+    console.log(req.body)
     const postNote = {
         title: req.body.title,
         text: req.body.text
